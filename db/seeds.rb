70211f8252ebda21a555192a16cb9db1a12dfd49
windy <yafei@dao42.com>
2025-09-06 20:52:05 +0800

    chore: add kaminari config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+
+# IMPORTANT: Do NOT add Administrator data here!
+# Administrator accounts should be created manually or through separate admin setup commands.
+# This seeds file is only for application data (products, categories, etc.)
+
 # Write your seed data here

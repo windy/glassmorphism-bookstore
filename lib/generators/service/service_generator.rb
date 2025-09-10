330d2156497827abe1bebccde9253925b7078de7
windy <yafei@dao42.com>
2025-09-10 16:56:54 +0800

    chore: service generator underscore

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -4,16 +4,34 @@ class ServiceGenerator < Rails::Generators::NamedBase
   desc "Generate a service class"
 
   def create_service_file
-    template 'service.rb.erb', "app/services/#{file_name}_service.rb"
+    template 'service.rb.erb', "app/services/#{file_name}.rb"
   end
 
   private
 
   def file_name
-    name.underscore
+    if name.downcase.end_with?('service')
+      base_name = name.gsub(/service$/i, '')
+      standardized_base = standardize_class_name(base_name).underscore
+      "#{standardized_base}_service"
+    else
+      standardized_name = standardize_class_name(name).underscore
+      "#{standardized_name}_service"
+    end
   end
 
   def class_name
-    "#{name.classify}Service"
+    if name.downcase.end_with?('service')
+      base_name = name.gsub(/service$/i, '')
+      standardized_base = standardize_class_name(base_name)
+      "#{standardized_base}Service"
+    else
+      standardized_name = standardize_class_name(name)
+      "#{standardized_name}Service"
+    end
+  end
+
+  def standardize_class_name(name)
+    name.underscore.classify
   end
 end

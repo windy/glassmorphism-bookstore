62119b40e96bbb0e6c0d2f9836eac623f9718916
windy <yafei@dao42.com>
2025-09-15 22:32:27 +0800

    chore: fix copy error in iframe

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Myapp
         helper_specs: false,
         routing_specs: false,
         controller_specs: false,
-        request_specs: true
+        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
